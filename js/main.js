3bec81e207fe01dd3d7a80a78274e345635a2d47
Alisher778 <alisher.musurmonov89@gmail.com>
2017-07-26 23:58:04 -0400

    almost finished proposal page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,39 @@
-$(docuement).ready(function(){
+$(document).ready(function(){
 
 	$('[data-toggle="tooltip"]').tooltip({delay: { "show": 50, "hide": 100 }});
+	
+	var totalFee = 0;
+	$('.calculate').on('click', function(){
+		
+		var fee = $('.fee');
+
+		fee.map(function(feeList) {
+			var inputVal = parseInt(fee.eq(feeList).val(), 10);
+			if(fee.eq(feeList).val() === ""){
+				inputVal = 0;
+			}
+			totalFee+= inputVal
+			console.log(totalFee)
+			
+		});
+		$('.total >input').val(totalFee)
+	});
+
+	$('header').sticky({topSpacing:0, zIndex: 1000})
+    $('.datepicker').pickadate()
+
+
+	$('div.done').click(function(){
+			$('#mceu_15, div.done').hide();
+			$('div.edit').show().addClass('hide-on-print');
+			$('#textarea-result').html(tinyMCE.activeEditor.getContent());
+			
+		});
+
+		$('div.edit').click(function(){
+			$('#mceu_15').show();
+			$('div.done').show();
+			$('div.edit').hide();
+		});
+
 });
